cfcc548a1c2e4afabcba3bb98fe4400825deefab
Hakuxe <arthurnorton.jobs@gmail.com>
2022-06-28 14:11:08 -0300

    adicionando integração com graphql-code-generator

--- event-platform/src/pages/Event.tsx
+++ event-platform/src/pages/Event.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
-import { Video } from "../components/Video";
+import { VideoEvent } from "../components/VideoEvent";
 
 export function Event() {
 	const { slug } = useParams<{ slug: string }>();
@@ -9,7 +9,7 @@ export function Event() {
 		<div className="flex flex-col min-h-screen">
 			<Header />
 			<main className="flex flex-1">
-				{slug ? <Video lessonSlug={slug} /> : <div className="flex-1"></div>}
+				{slug ? <VideoEvent lessonSlug={slug} /> : <div className="flex-1"></div>}
 
 				<Sidebar />
 			</main>
