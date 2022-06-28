79aa91d044a88c2317c6d24bbbcdd15ad4275710
Hakuxe <arthurnorton.jobs@gmail.com>
2022-06-28 14:10:44 -0300

    substituindo vimejs  por react-player

--- event-platform/src/components/VideoEvent.tsx
+++ event-platform/src/components/VideoEvent.tsx
@@ -1,5 +1,5 @@
-import { gql, useQuery } from "@apollo/client";
-import { DefaultUi, Player, Youtube } from "@vime/react";
+// import { DefaultUi, Player, Youtube } from "@vime/react";
+import ReactPlayer from "react-player";
 import {
 	CaretRight,
 	DiscordLogo,
@@ -7,53 +7,21 @@ import {
 	Lightning,
 } from "phosphor-react";
 
-import "@vime/core/themes/default.css";
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-	query GetLessonBySlug($slug: String) {
-		lesson(where: { slug: $slug }) {
-			title
-			videoId
-			description
-			teacher {
-				bio
-				avatarURL
-				name
-			}
-		}
-	}
-`;
-
-interface GetLessonBySlugResponse {
-	lesson: {
-		title: string;
-		videoId: string;
-		description: string;
-		teacher: {
-			bio: string;
-			avatarURL: string;
-			name: string;
-		};
-	};
-}
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
 	lessonSlug: string;
 }
 
-export function Video(props: VideoProps) {
-	const { data } = useQuery<GetLessonBySlugResponse>(
-		GET_LESSON_BY_SLUG_QUERY,
-		{
-			variables: {
-				slug: props.lessonSlug,
-			},
-		}
-	);
+export function VideoEvent(props: VideoProps) {
+	const { data } = useGetLessonBySlugQuery({
+		variables: {
+			slug: props.lessonSlug,
+		},
+	});
 
-	console.log(data);
 
-	if (!data) {
+	if (!data || !data.lesson) {
 		return (
 			<div className="flex-1">
 				<p className="text-2xl flex justify-center">carregando...</p>
@@ -64,10 +32,18 @@ export function Video(props: VideoProps) {
 		<div className="flex-1">
 			<div className="bg-black flex justify-center">
 				<div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
-					<Player>
+					{/* <Player>
 						<Youtube videoId={data.lesson.videoId} />
 						<DefaultUi />
-					</Player>
+					</Player> */}
+
+					<ReactPlayer
+						url={`https://www.youtube.com/watch?v=${data.lesson.videoId}`}
+						controls={true}
+						width={"100%"}
+						height={"100%"}
+					/>
+					
 				</div>
 			</div>
 
@@ -79,21 +55,23 @@ export function Video(props: VideoProps) {
 							{data.lesson.description}
 						</p>
 
-						<div className="flex items-center gap-4 mt-6">
-							<img
-								className="h-16 w-16 rounded-full border-2 border-blue-500"
-								src={data.lesson.teacher.avatarURL}
-								alt=""
-							/>
-							<div className="leading-relaxed">
-								<strong className=" font-bold text-2xl block">
-									{data.lesson.teacher.name}
-								</strong>
-								<span className="text-gray-200 text-sm block">
-									{data.lesson.teacher.bio}
-								</span>
+						{data.lesson.teacher && (
+							<div className="flex items-center gap-4 mt-6">
+								<img
+									className="h-16 w-16 rounded-full border-2 border-blue-500"
+									src={data.lesson.teacher.avatarURL}
+									alt=""
+								/>
+								<div className="leading-relaxed">
+									<strong className=" font-bold text-2xl block">
+										{data.lesson.teacher.name}
+									</strong>
+									<span className="text-gray-200 text-sm block">
+										{data.lesson.teacher.bio}
+									</span>
+								</div>
 							</div>
-						</div>
+						)}
 					</div>
 					<div className="flex flex-col gap-4">
 						<a
