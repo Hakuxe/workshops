152da4c7bb1b439d0f326f6972cfa08cc5a7d1fa
Hakuxe <arthurnorton.jobs@gmail.com>
2022-08-16 15:06:53 -0300

    criptografando senha antes de inserir no bd

--- suport-app/src/services/CreateUserService.ts
+++ suport-app/src/services/CreateUserService.ts
@@ -1,15 +1,17 @@
 // services ou useCases => regras para manipular as entidades
 
 import { UserRepository } from "../repositories/UserRepositories";
+import { hash } from "bcryptjs";
 
 interface IUserRequest {
 	name: string;
 	email: string;
-	admin?: boolean ;
+	admin?: boolean;
+	password: string;
 }
 
 export class CreateUserService {
-	async execute({ email, name, admin }: IUserRequest) {
+	async execute({ email, name, admin, password }: IUserRequest) {
 		if (!email) {
 			throw new Error("Email Incorrect");
 		}
@@ -20,14 +22,22 @@ export class CreateUserService {
 			throw new Error("User Already exists");
 		}
 
+		// if (!password) {
+		// 	throw new Error("Password can't be empty");
+		// }
+
+		const passwordEncrypted = await hash(password, 8);
+
+
 		const newUser = UserRepository.create({
 			name,
 			email,
 			admin,
+			password: passwordEncrypted,
 		});
 
-      await UserRepository.save(newUser);
+		await UserRepository.save(newUser);
 
-      return(newUser)
+		return newUser;
 	}
 }
