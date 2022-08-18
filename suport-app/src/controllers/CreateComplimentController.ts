55c1b256700f429857cc50d62903f7bf525226bc
Hakuxe <arthurnorton.jobs@gmail.com>
2022-08-18 18:57:22 -0300

    corrigindo erro

--- suport-app/src/controllers/CreateComplimentController.ts
+++ suport-app/src/controllers/CreateComplimentController.ts
@@ -3,14 +3,16 @@ import { CreateComplimentService } from "../services/CreateComplimentService";
 
 export class CreateComplimentController {
 	async handle(request: Request, response: Response) {
-		const { message, user_sender, user_receiver, tag_id } = request.body;
+		const { message, user_receiver, tag_id } = request.body;
+
+		const { user_id } = request;
 
 		const createComplimentService = new CreateComplimentService();
 
 		const newCompliment = await createComplimentService.execute({
 			message,
+			user_sender: user_id,
 			user_receiver,
-			user_sender,
 			tag_id,
 		});
 
