607cedbe9027306eb3232d03d174b1053808016b
Hakuxe <arthurnorton.jobs@gmail.com>
2022-09-15 22:19:29 -0300

    estruturando rotas base

--- eSports/server/src/server.ts
+++ eSports/server/src/server.ts
@@ -1,9 +1,39 @@
-import express from "express";
+import express, { application, response } from "express";
 
 const app = express();
 
-app.listen(3333, () => console.log("server running...."))
+// middleware
+app.use(express.json());
 
+app.listen(3333, () => console.log("server running...."));
 
-app.get("/", (req, res) => res.send("hello word\n"))
+// criar anúncio
+app.post("/ads", (request, response) => {
+	return response.status(201).json([]);
+});
 
+// listar games
+app.get("/games", (request, response) => {
+	return response.json([]);
+});
+
+//  listar anúncios de um game
+app.get("/games/:id/ads", (request, response) => {
+   // const gameId = request.params.id;
+
+	return response.json([
+		{ id: 1, name: "csgo" },
+		{ id: 2, name: "fortnite" },
+		{ id: 3, name: "valorant" },
+	]);
+});
+
+//buscar discord por id do anúncio
+app.get("/games/:id/discord", (request, response)=>{
+   return response.json([
+		{ id: 1, name: "csgo" },
+		{ id: 2, name: "fortnite" },
+		{ id: 3, name: "valorant" },
+	]);
+
+})
