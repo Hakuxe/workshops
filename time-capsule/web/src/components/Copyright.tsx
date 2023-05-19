0aaf7a77d047cf5aa22ae47de546abebef0bc70c
Hakuxe <arthurnorton.jobs@gmail.com>
2023-05-19 17:05:54 -0300

    creating front for time-capsule

---
+++ time-capsule/web/src/components/Copyright.tsx
@@ -0,0 +1,17 @@
+export function Copyright() {
+  return (
+    <footer className="text-sm leading-relaxed text-gray-200">
+      <p>
+        Feito com 💜 no NLW da{" "}
+        <a
+          href=""
+          rel="noreferrer"
+          target="_blank"
+          className="underline hover:text-gray-100"
+        >
+          Rocketseat
+        </a>
+      </p>
+    </footer>
+  );
+}
